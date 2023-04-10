22a150a9240fc171dad6bc82ab934d9de946559c
themanyone <nospam@thenerdshow.com>
2023-04-10 12:35:43 -0800

    commit python3 fork

---
+++ README.txt
@@ -0,0 +1,161 @@
+Interactive TCC
+===============
+
+A real-eval-print loop (REPL) for C/C++ programmers, written in Python.
+
+Interactive TCC (itcc) is a python3 fork of Interactive GCC (igcc) https://codeberg.org/andybalaam/igcc with TCC compiler option.
+
+Get optional TCC compiler here:
+ git clone https://repo.or.cz/tinycc.git/
+
+The advantage of using TCC is speed. There are no compiler delays between entering code and seeing the results! But it lacks some of the functionality of GCC. Also, TCC is a C compiler, not a C++ compiler like GCC.
+
+Use Interactive TCC for C programming, like this:
+
+ $ ./itcc
+ tcc> int a = 5;
+ tcc> int a -= 2;
+ tcc> printf("a is %i\n", a);
+ 3
+
+Interactive TCC works about like Interactive GCC (igcc), which is also included in this package. If you can convert C++ to C, you might even be able to struggle through some of following examples using itcc.
+ 
+Interactive GCC
+===============
+
+Use Interactive GCC for C++ programming, like this:
+
+ $ ./igcc 
+ g++> int a = 5;
+ g++> a += 2;
+ g++> cout << a << endl;
+ 7
+ g++> --a;
+ g++> cout << a << endl;
+ 6
+ g++> 
+
+It is possible to include header files you need like this:
+
+ $ ./igcc 
+ g++> #include <vector>
+ g++> vector<int> myvec;
+ g++> myvec.push_back( 17 );
+ g++> printf( "%d\n", myvec.size() );
+ 1
+ g++> myvec.push_back( 21 );
+ g++> printf( "%d\n", myvec.size() );
+ 2
+ g++> 
+
+Compile errors can be tolerated until the code works:
+
+ $ ./igcc
+ g++> #include <map>
+ g++> map<string,int> hits;
+ g++> hits["foo"] = 12;
+ g++> hits["bar"] = 15;
+ g++> for( map<string,int>::iterator it = hits.begin(); it != hits.end(); ++it )
+ [Compile error - type .e to see it.]
+ g++> {
+ [Compile error - type .e to see it.]
+ g++> 	cout << it->first << " " << it->second << endl;
+ [Compile error - type .e to see it.]
+ g++> }
+ bar 15
+ foo 12
+ g++> 
+
+Extra include directories can be supplied:
+
+ $ ./igcc -Itest/cpp -Itest/cpp2
+ g++> #include "hello.h"
+ g++> hello();
+ Hello, 
+ g++> #include "world.h"
+ g++> world();
+ world!
+ g++> 
+
+Libs can be linked:
+
+ $ ./igcc -lm
+ g++> #include "math.h"
+ g++> cout << pow( 3, 3 ) << endl; // Actually a bad example since libm.a is already linked in C++
+ 27
+ g++> 
+
+Your own libs can be linked too:
+
+ $ ./igcc -Itest/cpp -Ltest/cpp -lmylib
+ g++> #include "mylib.h"
+ g++> defined_in_cpp();
+ defined_in_cpp saying hello.
+ g++> 
+
+The cstdio, iostream and string headers are automatically included, and the std namespace is automatically in scope.
+
+Downloading and using
+---------------------
+
+Download the IGCC tarball from the Sourceforge download area:
+
+https://sourceforge.net/projects/igcc/files/
+
+Untar it like so:
+
+ tar -xjf igcc-0.1.tar.bz2
+
+And then start the program like this:
+
+ cd igcc-0.1
+ ./igcc
+
+Then type the C++ code you want to execute. It will be compiled with GCC and the results (if any) will be displayed.
+
+Type .h to see some (minimal) help.
+
+Developing
+----------
+
+IGCC is a small python wrapper around GCC.
+
+Check out the code here:
+
+ git clone git://igcc.git.sourceforge.net/gitroot/igcc/igcc
+
+Or browse the source here:
+
+http://igcc.git.sourceforge.net/git/gitweb.cgi?p=igcc/igcc;a=tree
+
+Links
+-----
+
+IGCC home page:
+http://www.artificialworlds.net/wiki/IGCC/IGCC
+
+IGCC Sourceforge page:
+http://sourceforge.net/projects/igcc/
+
+Andy Balaam's home page:
+http://www.artificialworlds.net
+
+Andy Balaam's blog:
+http://www.artificialworlds.net/blog
+
+Contact
+-------
+
+Andy Balaam may be contacted on axis3x3 at users dot sourceforge dot net
+
+Copyright
+---------
+
+IGCC is Copyright (C) 2009 Andy Balaam
+
+IGCC is Free Software released under the terms of the GNU General Public License version 2 or later.
+
+IGCC comes with NO WARRANTY.
+
+See the file COPYING for more information.
+
---
+++ libigcc/runtcc.py
@@ -0,0 +1,300 @@
+# itcc - a read-eval-print loop for C/C++ programmers
+#
+# Copyright (C) 2009 Andy Balaam
+# with python3 and tcc support by Henry Kroll III
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+import itertools
+import os
+import os.path
+import re
+import readline
+import subprocess
+import sys
+import tempfile
+from optparse import OptionParser
+
+from . import dot_commands
+from . import source_code_c as source_code
+from . import version
+
+# --------------
+
+# One day these will be in a config file
+
+prompt = "tcc> "
+compiler_command = ( "tcc", "-std=c11", "-x", "c", "-o", "$outfile", "-",
+	"$include_dirs", "$lib_dirs", "$libs" )
+
+include_dir_command = ( "-I$cmd", )
+lib_dir_command = ( "-L$cmd", )
+lib_command = ( "-l$cmd", )
+
+#---------------
+
+incl_re = re.compile( r"\s*#\s*include\s" )
+
+#---------------
+
+def read_line_from_stdin( prompt ):
+	try:
+		return input( prompt )
+	except EOFError:
+		return None
+
+def read_line_from_file( inputfile, prompt ):
+	sys.stdout.write( prompt )
+	line = inputfile.readline()
+	if line is not None:
+		print(line)
+	return line
+
+def create_read_line_function( inputfile, prompt ):
+	if inputfile is None:
+		return lambda: read_line_from_stdin( prompt )
+	else:
+		return lambda: read_line_from_file( inputfile, prompt )
+
+def get_temporary_file_name():
+	outfile = tempfile.NamedTemporaryFile( prefix = 'igcc-exe' )
+	outfilename = outfile.name
+	outfile.close()
+	return outfilename
+
+def append_multiple( single_cmd, cmdlist, ret ):
+	if cmdlist is not None:
+		for cmd in cmdlist:
+			for cmd_part in single_cmd:
+				ret.append(
+					cmd_part.replace( "$cmd" , cmd ) )
+
+def get_compiler_command( options, outfilename ):
+	ret = []
+	for part in compiler_command:
+		if part == "$include_dirs":
+			append_multiple( include_dir_command, options.INCLUDE, ret )
+		elif part == "$lib_dirs":
+			append_multiple( lib_dir_command, options.LIBDIR,ret )
+		elif part == "$libs":
+			append_multiple( lib_command, options.LIB, ret )
+		else:
+			ret.append( part.replace( "$outfile", outfilename ) )
+	return ret
+
+
+def run_compile( subs_compiler_command, runner ):
+	#print "$ " + ( " ".join( subs_compiler_command ) )
+	compile_process = subprocess.Popen( subs_compiler_command,
+		stdin = subprocess.PIPE, stderr = subprocess.PIPE )
+	stdoutdata, stderrdata = compile_process.communicate(
+		source_code.get_full_source( runner ).encode('utf-8') )
+
+	if compile_process.returncode == 0:
+		return None
+	elif stdoutdata is not None:
+		if stderrdata is not None:
+			return stdoutdata + stderrdata
+		else:
+			return stdoutdata
+	else:
+		if stderrdata is not None:
+			return stderrdata
+		else:
+			return "Unknown compile error - compiler did not write any output."
+		
+
+def run_exe( exefilename ):
+	run_process = subprocess.Popen( exefilename,
+		stdout = subprocess.PIPE, stderr = subprocess.PIPE )
+	return run_process.communicate()
+
+def print_welcome():
+	print('''igcc $version
+Released under GNU GPL version 2 or later, with NO WARRANTY.
+Type ".h" for help.
+'''.replace( "$version", version.VERSION ))
+
+class UserInput:
+	INCLUDE = 0
+	COMMAND = 1
+
+	def __init__( self, inp, typ ):
+		self.inp = inp
+		self.typ = typ
+		self.output_chars = 0
+		self.error_chars = 0
+
+	def __str__( self ):
+		return "UserInput( '%s', %d, %d, %d )" % (
+			self.inp, self.typ, self.output_chars, self.error_chars )
+
+	def __eq__( self, other ):
+		return (
+			self.inp == other.inp and
+			self.typ == other.typ and
+			self.output_chars == other.output_chars and
+			self.error_chars == other.error_chars )
+
+	def __ne__( self, other ):
+		return not self.__eq__( other )
+
+class Runner:
+
+	def __init__( self, options, inputfile, exefilename ):
+		self.options = options
+		self.inputfile = inputfile
+		self.exefilename = exefilename
+		self.user_input = []
+		self.input_num = 0
+		self.compile_error = ""
+		self.output_chars_printed = 0
+		self.error_chars_printed = 0
+
+	def do_run( self ):
+		read_line = create_read_line_function( self.inputfile, prompt )
+
+		subs_compiler_command = get_compiler_command( self.options, self.exefilename )
+
+		inp = 1
+		while inp is not None:
+			inp = read_line()
+			if inp is not None:
+
+				col_inp, run_cmp = (
+					dot_commands.process( inp, self ) )
+				if col_inp:
+					if self.input_num < len( self.user_input ):
+						self.user_input = self.user_input[ : self.input_num ]
+					if incl_re.match( inp ):
+						typ = UserInput.INCLUDE
+					else:
+						typ = UserInput.COMMAND
+					self.user_input.append( UserInput( inp, typ ) )
+					self.input_num += 1
+
+				if run_cmp:
+					self.compile_error = run_compile( subs_compiler_command,
+						self )
+
+					if self.compile_error is not None:
+						# print("[Compile error - type .e to see it.]")
+						print(self.compile_error.decode('utf-8').strip().split('\n'))
+					else:
+						stdoutdata, stderrdata = run_exe( self.exefilename )
+
+						if len( stdoutdata ) > self.output_chars_printed:
+							new_output = stdoutdata[self.output_chars_printed:]
+							len_new_output = len( new_output )
+							print(new_output.decode('utf-8'))
+							self.output_chars_printed += len_new_output
+							self.user_input[ -1 ].output_chars = len_new_output
+
+						if len( stderrdata ) > self.error_chars_printed:
+							new_error = stderrdata[self.error_chars_printed:]
+							len_new_error = len( new_error )
+							print(new_error)
+							self.error_chars_printed += len_new_error
+							self.user_input[ -1 ].error_chars = len_new_error
+
+		print()
+
+	def redo( self ):
+		if self.input_num < len( self.user_input ):
+			self.input_num += 1
+			return self.user_input[ self.input_num - 1 ].inp
+		else:
+			return None
+
+
+	def undo( self ):
+		if self.input_num > 0:
+			self.input_num -= 1
+			undone_input = self.user_input[ self.input_num ]
+			self.output_chars_printed -= undone_input.output_chars
+			self.error_chars_printed -= undone_input.error_chars
+			return undone_input.inp
+		else:
+			return None
+
+	def get_user_input( self ):
+		return itertools.islice( self.user_input, 0, self.input_num )
+
+	def get_user_commands( self ):
+		return ( a.inp for a in filter(
+			lambda a: a.typ == UserInput.COMMAND,
+			self.get_user_input() ) )
+
+	def get_user_includes( self ):
+		return ( a.inp for a in filter(
+			lambda a: a.typ == UserInput.INCLUDE,
+			self.get_user_input() ) )
+
+	def get_user_commands_string( self ):
+		return "\n".join( self.get_user_commands() ) + "\n"
+
+	def get_user_includes_string( self ):
+		return "\n".join( self.get_user_includes() ) + "\n"
+
+def parse_args( argv ):
+	parser = OptionParser( version="igcc " + version.VERSION )
+			
+	parser.add_option( "-I", "", dest="INCLUDE", action="append",
+		help = "Add INCLUDE to the list of directories to " +
+			"be searched for header files." )
+	parser.add_option( "-L", "", dest="LIBDIR", action="append",
+		help = "Add LIBDIR to the list of directories to " +
+			"be searched for library files." )
+	parser.add_option( "-l", "", dest="LIB", action="append",
+		help = "Search the library LIB when linking." )
+		
+	(options, args) = parser.parse_args( argv )
+
+	if len( args ) > 0:
+		parser.error( "Unrecognised arguments :" +
+			" ".join( arg for arg in args ) )
+
+	return options
+
+def run( outputfile = sys.stdout, inputfile = None, print_welc = True,
+		argv = None ):
+
+	# TODO: replace try...finally with a "with" statement
+	real_sys_stdout = sys.stdout
+	sys.stdout = outputfile
+
+	exefilename = ""
+
+	try:
+		try:
+			options = parse_args( argv )
+
+			exefilename = get_temporary_file_name()
+			ret = "normal"
+			if print_welc:
+				print_welcome()
+			Runner( options, inputfile, exefilename ).do_run()
+		except Exception as e:
+			if e == dot_commands.IGCCQuitException:
+				ret = "quit"
+	finally:
+		sys.stdout = real_sys_stdout
+
+		if os.path.isfile( exefilename ):
+			os.remove( exefilename )
+
+	return ret
+
